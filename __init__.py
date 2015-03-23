dfa135fdcea9bf785484152425ec89a522fda3a6
Richard Huang <rickypc@users.noreply.github.com>
2015-03-23 08:45:22 -0700

    Add back parent library documentation.

--- __init__.py
+++ __init__.py
@@ -25,8 +25,81 @@ __version__ = get_version()
 
 
 class ExtendedSelenium2Library(Selenium2Library.Selenium2Library):
-    """Extended Selenium2 library to support AngularJS and custom improvement to be used with
-    Robot Framework's test library.
+    """ExtendedSelenium2Library is a web testing library with AngularJS support and
+    custom improvement for Robot Framework.
+
+    It uses the Selenium 2 (WebDriver) libraries internally to control a web browser.
+    See http://seleniumhq.org/docs/03_webdriver.html for more information on Selenium 2
+    and WebDriver.
+
+    ExtendedSelenium2Library runs tests in a real browser instance. It should work in
+    most modern browsers and can be used with both Python and Jython interpreters.
+
+    = Before running tests =
+
+    Prior to running test cases using ExtendedSelenium2Library, ExtendedSelenium2Library must be
+    imported into your Robot test suite (see `importing` section), and the
+    `Open Browser` keyword must be used to open a browser to the desired location.
+
+    = Locating elements =
+
+    All keywords in ExtendedSelenium2Library that need to find an element on the page
+    take an argument, `locator`. By default, when a locator value is provided,
+    it is matched against the key attributes of the particular element type.
+    For example, `id` and `name` are key attributes to all elements, and
+    locating elements is easy using just the `id` as a `locator`. For example::
+
+    Click Element  my_element
+
+    It is also possible to specify the approach ExtendedSelenium2Library should take
+    to find an element by specifying a lookup strategy with a locator
+    prefix. Supported strategies are:
+
+    | *Strategy*   | *Example*                                       | *Description*                                      |
+    | identifier   | Click Element `|` identifier=my_element         | Matches by @id or @name attribute                  |
+    | id           | Click Element `|` id=my_element                 | Matches by @id attribute                           |
+    | name         | Click Element `|` name=my_element               | Matches by @name attribute                         |
+    | xpath        | Click Element `|` xpath=//div[@id='my_element'] | Matches with arbitrary XPath expression            |
+    | dom          | Click Element `|` dom=document.images[56]       | Matches with arbitrary DOM express                 |
+    | link         | Click Element `|` link=My Link                  | Matches anchor elements by their link text         |
+    | partial link | Click Element `|` partial link=y Lin            | Matches anchor elements by their partial link text |
+    | css          | Click Element `|` css=div.my_class              | Matches by CSS selector                            |
+    | jquery       | Click Element `|` jquery=div.my_class           | Matches by jQuery/sizzle selector                  |
+    | sizzle       | Click Element `|` sizzle=div.my_class           | Matches by jQuery/sizzle selector                  |
+    | tag          | Click Element `|` tag=div                       | Matches by HTML tag name                           |
+    | default*     | Click Link    `|` default=page?a=b              | Matches key attributes with value after first '='  |
+    * Explicitly specifying the default strategy is only necessary if locating
+    elements by matching key attributes is desired and an attribute value
+    contains a '='. The following would fail because it appears as if _page?a_
+    is the specified lookup strategy:
+    | Click Link    page?a=b
+    This can be fixed by changing the locator to:
+    | Click Link    default=page?a=b
+
+    Table related keywords, such as `Table Should Contain`, work differently.
+    By default, when a table locator value is provided, it will search for
+    a table with the specified `id` attribute. For example:
+
+    Table Should Contain  my_table  text
+
+    More complex table lookup strategies are also supported:
+
+    | *Strategy* | *Example*                                                          | *Description*                     |
+    | css        | Table Should Contain `|` css=table.my_class `|` text               | Matches by @id or @name attribute |
+    | xpath      | Table Should Contain `|` xpath=//table/[@name="my_table"] `|` text | Matches by @id or @name attribute |
+
+    = Timeouts =
+
+    There are several `Wait ...` keywords that take timeout as an
+    argument. All of these timeout arguments are optional. The timeout
+    used by all of them can be set globally using the
+    `Set Selenium Timeout` keyword. The same timeout also applies to
+    `Execute Async Javascript`.
+
+    All timeouts can be given as numbers considered seconds (e.g. 0.5 or 42)
+    or in Robot Framework's time syntax (e.g. '1.5 seconds' or '1 min 30 s').
+    For more information about the time syntax see:
+    http://robotframework.googlecode.com/svn/trunk/doc/userguide/RobotFrameworkUserGuide.html#time-format.
     """
 
     NG_WRAPPER = '%(prefix)s' \
@@ -36,6 +109,33 @@ class ExtendedSelenium2Library(Selenium2Library.Selenium2Library):
     ROBOT_LIBRARY_SCOPE = 'TEST SUITE'
 
     def __init__(self, timeout=5.0, implicit_wait=0.0, run_on_failure='Capture Page Screenshot'):
+        """ExtendedSelenium2Library can be imported with optional arguments.
+
+        `timeout` is the default timeout used to wait for all waiting actions.
+        It can be later set with `Set Selenium Timeout`.
+
+        'implicit_wait' is the implicit timeout that Selenium waits when
+        looking for elements.
+        It can be later set with `Set Selenium Implicit Wait`.
+        See `WebDriver: Advanced Usage`__ section of the SeleniumHQ documentation
+        for more information about WebDriver's implicit wait functionality.
+
+        __ http://seleniumhq.org/docs/04_webdriver_advanced.html#explicit-and-implicit-waits
+
+        `run_on_failure` specifies the name of a keyword (from any available
+        libraries) to execute when a ExtendedSelenium2Library keyword fails. By default
+        `Capture Page Screenshot` will be used to take a screenshot of the current page.
+        Using the value "Nothing" will disable this feature altogether. See
+        `Register Keyword To Run On Failure` keyword for more information about this
+        functionality.
+
+        Examples:
+        | Library `|` ExtendedSelenium2Library `|` 15                                            | # Sets default timeout to 15 seconds                                       |
+        | Library `|` ExtendedSelenium2Library `|` 0 `|` 5                                       | # Sets default timeout to 0 seconds and default implicit_wait to 5 seconds |
+        | Library `|` ExtendedSelenium2Library `|` 5 `|` run_on_failure=Log Source               | # Sets default timeout to 5 seconds and runs `Log Source` on failure       |
+        | Library `|` ExtendedSelenium2Library `|` implicit_wait=5 `|` run_on_failure=Log Source | # Sets default implicit_wait to 5 seconds and runs `Log Source` on failure |
+        | Library `|` ExtendedSelenium2Library `|` timeout=10      `|` run_on_failure=Nothing    | # Sets default timeout to 10 seconds and does nothing on failure           |
+        """
         Selenium2Library.Selenium2Library.__init__(self, timeout, implicit_wait, run_on_failure)
         #self._element_finder = ExtendedElementFinder()
 
@@ -312,7 +412,8 @@ class ExtendedSelenium2Library(Selenium2Library.Selenium2Library):
             if invisible:
                 return
             elif invisible is None:
-                return error or "Element locator '%s' did not match any elements after %s" % (locator, self._format_timeout(timeout))
+                return error or "Element locator '%s' did not match any elements after %s" %\
+                                (locator, self._format_timeout(timeout))
             else:
                 return error or "Element '%s' was visible in %s" % (locator, self._format_timeout(timeout))
         self._wait_until_no_error(timeout, check_invisibility)
