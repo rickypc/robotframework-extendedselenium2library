99a082c6c956c24ae3673e4b4c37d369a9963de6
Richard Huang <rickypc@users.noreply.github.com>
2015-07-10 23:30:42 -0700

    Add pylint rc file, add entry in make file, and adjust the module.

--- src/ExtendedSelenium2Library/version.py
+++ src/ExtendedSelenium2Library/version.py
@@ -17,8 +17,13 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-VERSION = '0.4.10'
+"""
+Extended Selenium2 Library - a web testing library with AngularJS support.
+"""
+
+VERSION = '0.4.11'
 
 
 def get_version():
+    """Returns the current version."""
     return VERSION
